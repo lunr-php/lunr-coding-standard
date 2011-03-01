7a021e2c0104ce2bf2d6ea0a59b896e922829dd2
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-01 23:56:31 +0100

    Initial Import of a basic Lunr Coding Standard Definition

---
+++ LunrCodingStandard.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Lunr Coding Standard
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Heinz Wiesinger
+ */
+
+if (class_exists( 'PHP_CodeSniffer_Standards_CodingStandard', true ) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+}
+
+/**
+ * Lunr Coding Standard
+ *
+ * Return a selection of default sniffs, followed by everything in the Lunr directory
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Heinz Wiesinger
+ */
+class PHP_CodeSniffer_Standards_Lunr_LunrCodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
+{
+    public function getExcludedSniffs()
+    {
+        return array(
+            'Squiz/Sniffs/WhiteSpace/ScopeIndentSniff.php',
+            'Squiz/Sniffs/WhiteSpace/ControlStructureSpacingSniff.php',
+            'Squiz/Sniffs/WhiteSpace/ScopeClosingBraceSniff.php',
+            'Squiz/Sniffs/WhiteSpace/LanguageConstructSpacingSniff.php',
+            'Squiz/Sniffs/WhiteSpace/FunctionSpacingSniff.php',
+            'Squiz/Sniffs/WhiteSpace/FunctionClosingBraceSpaceSniff.php',
+            'Squiz/Sniffs/WhiteSpace/OperatorSpacingSniff.php',
+            'Squiz/Sniffs/WhiteSpace/MemberVarSpacingSniff.php'
+        );
+    }
+
+    public function getIncludedSniffs()
+    {
+        return array(
+                '/PEAR/Sniffs/Functions/FunctionCallArgumentSpacingSniff.php',
+
+                'Lunr/Sniffs'
+                );
+    }
+}
+
+?>
