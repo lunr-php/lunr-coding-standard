447d16fc53168566943d67470dedd9dd131c08e7
Heinz Wiesinger <heinz.wiesinger@moveagency.com>
2023-06-16 21:52:45 +0200

    Remove useless assignment in FileCommentSniff

--- Lunr/Sniffs/Commenting/FileCommentSniff.php
+++ Lunr/Sniffs/Commenting/FileCommentSniff.php
@@ -68,8 +68,6 @@ class FileCommentSniff implements Sniff
      */
     public function process(File $phpcsFile, $stackPtr)
     {
-        $this->currentFile = $phpcsFile;
-
         $tokens       = $phpcsFile->getTokens();
         $commentStart = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
 
